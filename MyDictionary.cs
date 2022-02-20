a3a384ee59a2850b53b77034277694fb875df642
Tuba ARSLAN <arslanawzentuba1@gmail.com>
2022-02-20 22:50:13 +0300

    denendi.

---
+++ MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<T,U>
+    {
+        T[] dictionarykey;
+        U[] dictionaryvalue;
+
+        public MyDictionary()
+        {
+            dictionarykey = new T[0];
+            dictionaryvalue = new U[0];
+        }
+
+        public void Add(T anahtar, U deger)
+        {
+            T[] tempkey = dictionarykey;
+            U[] tempvalue = dictionaryvalue;
+
+            dictionarykey = new T[dictionarykey.Length + 1];
+            dictionaryvalue = new U[dictionaryvalue.Length + 1];
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                dictionarykey[i] = tempkey[i];
+                dictionaryvalue[i] = tempvalue[i];
+            }
+
+            dictionarykey[dictionarykey.Length - 1] = anahtar;
+            dictionaryvalue[dictionaryvalue.Length - 1] = deger;
+
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+
+            dictionary.Add("Tubiş", 1);
+            dictionary.Add("Fırat", 2);
+            dictionary.Add("Tuna", 3);
+        }
+    }
+}
